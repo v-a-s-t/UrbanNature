525dd664c7239a60e7a440427b07b8af2b31773b
andysheen <andymsheen@gmail.com>
2022-02-17 14:38:09 +0000

    increased PMS sample time

--- Sensors.ino
+++ Sensors.ino
@@ -318,7 +318,7 @@ void testSampleAllSensors() {
     //sampleMicLoudness();
   }
   if ((sensorFeeds.containsKey("sensor_p03um")) || (sensorFeeds.containsKey("sensor_p05um")) || (sensorFeeds.containsKey("sensor_p10um")) || (sensorFeeds.containsKey("sensor_p25um")) || (sensorFeeds.containsKey("sensor_p50um")) || (sensorFeeds.containsKey("sensor_p100um")) || (sensorFeeds.containsKey("sensor_pm10")) || (sensorFeeds.containsKey("sensor_pm25")) || (sensorFeeds.containsKey("sensor_pm100"))) {
-    for (int i = 0; i < 45; i ++) {
+    for (int i = 0; i < 100; i ++) {
       if (!readPMS5003()) {
         particulateData.particles_03um = prev_particles_03um;
         particulateData.particles_05um = prev_particles_05um;
